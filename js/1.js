74b409a2a64b29d6d7f80b0f1f01e6ce3bd6b24d
leticia <133894991+13martinlele@users.noreply.github.com>
2023-10-09 13:11:18 -0300

    Delete js directory

--- js/1.js
+++
@@ -1,9 +0,0 @@
-function musicas(){
-    var qtdM = parseInt(document.getElementById('qtdM').value);
-    var vezesM = parseInt(document.getElementById('vezesM').value);
-    const texto = document.querySelector("h3");
-
-    while(vezesM >= vezesM){
-        texto.innerHTML = vezesM;
-    }
-}
--- js/main.js
+++
@@ -1,9 +0,0 @@
-function desafio(){
-    const codigo = document.querySelector('#codigo').value;
-    if (codigo == 1){
-        window.location.href = "./html/desafio1.html"
-    }
-    else if(codigo == 2){
-        window.location.href = "./html/desafio2.html"
-    }
-}
